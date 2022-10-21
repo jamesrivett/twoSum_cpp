3687a89bf3c34fec6c78b72b400bb58484488c77
jamesrivett <jrivett@students.kennesaw.edu>
2022-10-21 00:41:54 -0400

    removing debug prints

--- twoSumLinear.cpp
+++ twoSumLinear.cpp
@@ -19,7 +19,6 @@ vector<int> twoSumLinear(vector<int> nums, int target) {
 
     while (top != bottom) {
         // Return if we've found the answer
-        cout << "result: " + to_string(sorted[bottom] + sorted[top]) + "\n";
         if (sorted[bottom] + sorted[top] == target) {
             answers.push_back(bottom);
             answers.push_back(top);
@@ -29,13 +28,11 @@ vector<int> twoSumLinear(vector<int> nums, int target) {
         // Iterate bottom pointer if result is too low
         else if (sorted[bottom] + sorted[top] < target) {
             bottom++;
-            cout << "iterated \n";
         }
 
         // De-Iterate top pointer if result is too high
         else if (sorted[bottom] + sorted[top] > target) {
             top--;
-            cout << "deiterated \n";
         }
 
     }
@@ -47,7 +44,7 @@ vector<int> twoSumLinear(vector<int> nums, int target) {
 
 int main(int c, char *v[]) {
     vector<int> input = vector<int>({0,1,2,7,11,15});
-    vector<int> answer = twoSumLinear(input, 26);
+    vector<int> answer = twoSumLinear(input, 9);
 
     cout << to_string(answer[0]) + ", " + to_string(answer[1]) + "\n";
 }
